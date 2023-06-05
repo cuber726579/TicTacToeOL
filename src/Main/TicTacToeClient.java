daf5b00ced6702180effd316a054d3e4cd0479c2
cuber726579 <2468956302@qq.com>
2023-06-05 17:22:48 +0800

    GUI

--- src/Main/TicTacToeClient.java
+++ src/Main/TicTacToeClient.java
@@ -1,5 +1,5 @@
 package Main;
-//dfjbssdjfksfhedgjkgsfjkdhfdgjkhs
+
 import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
@@ -9,9 +9,10 @@ import java.util.Scanner;
  */
 public class TicTacToeClient {
     private static final int SERVER_PORT = 8888;
-    private static BufferedReader inFromServer;
+    private static DataInputStream in;
+    private static DataOutputStream out;
+
     private static Scanner inFromSystem;
-    private static PrintWriter out;
 
     public static void main(String[] args) {
         System.out.println("Try to connect....");
@@ -21,32 +22,12 @@ public class TicTacToeClient {
              OutputStream outputStream = client.getOutputStream()) {
             System.out.println("Connect successfully....");
             // Turn streams into scanners and writers.
-            inFromServer = new BufferedReader(new InputStreamReader(inputStream));
-            out = new PrintWriter(outputStream);
+            in = new DataInputStream(inputStream);
+            out = new DataOutputStream(outputStream);
             inFromSystem = new Scanner(System.in);
-            new TicTacToeGUI(100,inFromServer,out,null);
+            new TicTacToeGUI(100,in,out,null);
         } catch (Exception e) {
             e.printStackTrace();
-        }
-    }
-
-    private static void playGame(Socket client) throws IOException, InterruptedException {
-        while (true) {
-            // Server sends message part.
-            String msg = null;
-            while ((msg = inFromServer.readLine()) != null) {
-                // Read message until "-1"
-                if (msg.equals("-1"))
-                    break;
-                System.out.println(msg);
-            }
-            // Client sends message part.
-            String command = inFromSystem.nextLine();
-            if (command.equals("EXIT")) {
-                return;
-            }
-            out.println(command);
-            out.flush();
         }
     }
 }
